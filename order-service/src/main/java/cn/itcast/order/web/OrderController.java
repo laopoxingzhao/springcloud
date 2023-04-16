63eef00fbdaca7bf16955b45b0d22c8611cf59b1
hu <laopo_one@outlook.com>
2023-04-16 21:45:48 +0800

    p7

---
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -0,0 +1,34 @@
+package cn.itcast.order.web;
+
+import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
+import cn.itcast.order.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("order")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+
+
+    @Autowired
+    RestTemplate restTemplate;
+    @GetMapping("{orderId}")
+    public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
+        // 根据id查询订单并返回
+        Order order = orderService.queryOrderById(orderId);
+        System.out.println(order);
+    String u= "http://localhost:8081/user/" + order.getUserId();
+        User object = restTemplate.getForObject(u, User.class);
+        order.setUser(object);
+        return order;
+}
+}
